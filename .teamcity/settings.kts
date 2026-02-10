3a62c0b18e34a5205d5dc148ba7cc33f9bb37449
Alexey Venderov <avenderov@gradle.com>
2026-02-10 13:29:22 +0100

    Add release TC job

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,42 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.CheckoutMode
+import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
+import jetbrains.buildServer.configs.kotlin.v2019_2.RelativeId
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2019_2.project
+import jetbrains.buildServer.configs.kotlin.v2019_2.toId
+import jetbrains.buildServer.configs.kotlin.v2019_2.version
 
-version = "2025.03"
+version = "2025.11"
 
 project {
+    buildType {
+        name = "Publish ANSI control sequence util"
+        id = RelativeId(name.toId())
+        description = "Publish ANSI control sequence util to Maven Central staging repository"
+
+        vcs {
+            root(DslContext.settingsRoot)
+            checkoutMode = CheckoutMode.ON_AGENT
+            cleanCheckout = true
+        }
+
+        requirements {
+            contains("teamcity.agent.jvm.os.name", "Linux")
+        }
+
+        steps {
+            gradle {
+                useGradleWrapper = true
+                tasks = "clean release"
+            }
+        }
+        params {
+            param("env.JDK8", "%linux.java8.oracle.64bit%")
+            param("env.JAVA_HOME", "%linux.java21.openjdk.64bit%")
+            param("env.MAVEN_CENTRAL_USERNAME", "%mavenCentralStagingRepoUser%")
+            password("env.MAVEN_CENTRAL_PASSWORD", "%mavenCentralStagingRepoPassword%")
+            password("env.PGP_SIGNING_KEY", "%pgpSigningKey%")
+            password("env.PGP_SIGNING_KEY_PASSPHRASE", "%pgpSigningPassphrase%")
+        }
+    }
 }
